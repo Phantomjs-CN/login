84d345b85c1e8f8bed74875fd1204562a6d7ae33
zanjs <root@zanjs.com>
2016-07-07 18:05:36 +0800

    page evaluate :haircut:

--- app.js
+++ app.js
@@ -4,13 +4,23 @@
 var page = require('webpage').create();
 
 
-page.open('http://biadu.com', function(status) {
-    console.log("Status: " + status);
+page.open('http://panli.com', function(status) {
+    console.log("Status2: " + status);
     if (status === "success") {
-        page.render('example.png');
+
+        //page.render('./images/panli.png'); //生成渲染后的界面图片
     }
+
+    var info = page.evaluate(function() {
+        var json = {
+            body: document.body,
+            title: document.title
+        }
+        return json;
+    });
+    console.log('Page info ' + JSON.stringify(info));
+
+
+
     phantom.exit();
 });
-
-
-
