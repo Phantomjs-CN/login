36559e618e66d9c30eb6e30d5b194b4d2f5fc6c4
zanjs <root@zanjs.com>
2016-07-14 16:16:06 +0800

    Every noble work is at first impossible :person_with_pouting_face:

--- casperjs/taobao.js
+++ casperjs/taobao.js
@@ -3,7 +3,7 @@ var casper = require('casper').create({
         loadImages: false,
 
         // 1.浏览器
-        userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
+        userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
     },
 
     // 2.浏览器窗口大小
